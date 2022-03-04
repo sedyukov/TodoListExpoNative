b49493fce19066404753c01e4f3a81d954e30ab8
Yaroslav Sedyukov <sedyukovofficial@gmail.com>
2022-03-04 16:17:43 +0300

    Added task creation function

--- components/task.js
+++ components/task.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, StyleSheet} from "react-native";
+import {CheckBox} from "react-native-web";
 
 const Task = (props) => {
+    const [isDone, setIsDone] = useState(props.isDone);
 
     return (
-        <View>
-            <Text>{props.text}</Text>
+        <View style={styles.taskWrapper}>
+            <CheckBox value={isDone} onValueChange={setIsDone}/>
+            <Text style={styles.taskText}>{props.text}</Text>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    taskWrapper: {
+        flex: 1,
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    taskText: {
+        fontSize: 12,
+        marginStart: 10
+    }
 });
+
 
 export default Task;
--- components/taskList.js
+++ components/taskList.js
@@ -1,31 +1,77 @@
 import React, {useCallback, useEffect, useState} from 'react'
-import { View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, ScrollView, Button, TextInput} from "react-native";
 import Task from "./task";
 
 const TaskList = () => {
     const [todos, setTodos] = useState([])
+    const [text, setText] = useState('')
 
 
     const fetchTodos = useCallback(async() => {
-        await fetch('https://jsonplaceholder.typicode.com/todos')
+        await fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5')
             .then(response => response.json())
             .then(json => setTodos(json))
     },[])
 
-    useEffect(() => {
-        fetchTodos()
-    }, [fetchTodos()])
+    const addTodo = () => {
+        if (!text) return;
+        setTodos([...todos,{"userId": 1,
+            "id": Date.now(),
+            "title": text,
+            "completed": false}])
+        setText('')
+    }
+
+
+    // useEffect(() => {
+    //     addTodo()
+    //     addTodo()
+    // }, [])
 
     return (
-        <View>
-            {
-                todos.map((todo) => {
-                    return (<Task text={todo.id} key={todo.id}/>)
-                })
-            }
+        <View style={styles.styleView}>
+            <TextInput
+                style={{
+                    height: 40,
+                    borderColor: 'gray',
+                    borderWidth: 1,
+                    paddingLeft: 10
+                }}
+                placeholder='Type new task'
+                onChange={event => {
+                    setText(event.target.value);
+                }}
+                value = {text}
+            />
+            <ScrollView style={styles.taskList}>
+                {
+                    todos.map((todo) => {
+                        return (<Task style={styles.styleTask} text={todo.title} isDone={todo.completed} key={todo.id}/>)
+                    })
+                }
+            </ScrollView>
+            <Button
+                style={styles.styleButton}
+                title = "Add task"
+                onPress={addTodo}
+            />
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    taskList: {
+        maxHeight: 100,
+        minHeight: 100,
+        marginBottom: 20
+    },
+    styleView: {
+        marginTop: 20
+    },
+    styleButton: {
+        color: 'black'
+    }
+
+})
 
 export default TaskList;
