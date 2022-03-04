1cae477a7bc3f0e4139ea15c202417b815462738
Yaroslav Sedyukov <sedyukovofficial@gmail.com>
2022-03-04 17:09:17 +0300

    Added delete function, but only for web works

--- components/task.js
+++ components/task.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, Image} from "react-native";
 import {CheckBox} from "react-native-web";
+import { AntDesign } from '@expo/vector-icons';
 
 const Task = (props) => {
     const [isDone, setIsDone] = useState(props.isDone);
+    const [isDeleted, setDeleted] =  useState(false)
 
+
+    if (isDeleted) return null;
     return (
         <View style={styles.taskWrapper}>
-            <CheckBox value={isDone} onValueChange={setIsDone}/>
-            <Text style={styles.taskText}>{props.text}</Text>
+            <View style={styles.startWrap}>
+                <CheckBox value={isDone} onValueChange={setIsDone}/>
+                <Text style={styles.taskText}>{props.text}</Text>
+            </View>
+            <AntDesign
+                name="delete"
+                size={16}
+                color="black"
+                onPress={()=>{setDeleted(true)}}
+            />
 
         </View>
     )
@@ -18,11 +30,16 @@ const styles = StyleSheet.create({
     taskWrapper: {
         flex: 1,
         flexDirection: 'row',
-        marginTop: 10
+        marginTop: 10,
+        justifyContent: 'space-between'
     },
     taskText: {
         fontSize: 12,
         marginStart: 10
+    },
+    startWrap: {
+        flex: 1,
+        flexDirection: 'row',
     }
 });
 
